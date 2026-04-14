b1744daf2381daf065c907e6f2534dc48f02b94f
Sergei Karlashov <velanishkis@yandex.ru>
2026-04-14 13:59:37 +0300

    Dve opechatki v fixturah ispravil, teper vse testi dolgni zapustitsja

--- conftest.py
+++ conftest.py
@@ -11,14 +11,14 @@ def driver():
 
 @pytest.fixture
 def rdata_for_good_reg():
-    new_name = f'Karlos{random.randit(100,999)}'
+    new_name = f'Karlos{random.randint(100,999)}'
     new_email = f'karlos{random.randint(100,999)}@mail.ru'
     new_password = f'karlos{random.randint(100,999)}'
     return new_name, new_email, new_password
 
 @pytest.fixture
 def rdata_for_badpass_reg():
-    new_name = f'Karlos{random.randit(100,999)}'
+    new_name = f'Karlos{random.randint(100,999)}'
     new_email = f'karlos{random.randint(100,999)}@mail.ru'
     new_password = f'k{random.randint(100,999)}'
     return new_name, new_email, new_password
