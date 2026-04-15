a1dd6fcc9156ee7f3dd2a2a9916f5e12c260511f
Sergei Karlashov <velanishkis@yandex.ru>
2026-04-15 18:57:25 +0300

    test_reg_new_acc dobavili assertov

--- tests/test_reg_new_acc.py
+++ tests/test_reg_new_acc.py
@@ -12,18 +12,24 @@ class TestRegistration:
         driver_base = MainPage(driver)
         driver_base.open(reg_url)
         new_name, new_email, new_password = rdata_for_good_reg
+        # вводим данные
         driver_base.type(Locators.username_input, new_name)
         driver_base.type(Locators.email_input, new_email)
         driver_base.type(Locators.password_input, new_password)
         driver_base.click(Locators.button_checkinn)
-        driver_base.find(Locators.button_login)
+        # Проверяем, что кнопка «Войти» появилась
+        assert driver_base.element_visible(Locators.button_login), "Кнопка 'Войти' не видна после регистрации"
+        # Выполняем вход
         driver_base.type(Locators.email_input, new_email)
         driver_base.type(Locators.password_input, new_password)
         driver_base.click(Locators.button_login)
-        driver_base.find(Locators.text_personal_accaunt)
+        # Переходим в личный кабинет
+        assert driver_base.element_visible(Locators.text_personal_accaunt), "Кнопка 'Личный кабинет' не видна на главной странице"
         driver_base.click(Locators.text_personal_accaunt)
-        driver_base.find(Locators.username_input)
-        assert driver_base.get_attribute_value(Locators.username_input, "value") == new_name
+        assert driver_base.element_visible(Locators.username_input), "Поле имени пользователя не видно в ЛК"
+        driver_base.find(Locators.username_input)  # поиск для взаимодействия
+        # Проверяем результат регистрации
+        assert driver_base.get_attribute_value(Locators.username_input, "value") == new_name, "Имя пользователя не соответствует ожидаемому"
 
     def test_invalid_password_by_registration(self, driver, rdata_for_badpass_reg):
         driver_base = MainPage(driver)
