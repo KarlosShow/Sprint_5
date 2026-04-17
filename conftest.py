4b451ab516f7515cd292956e274534e0fa68f95b
Sergei Karlashov <velanishkis@yandex.ru>
2026-04-17 18:08:56 +0300

    helpers.py i conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium import webdriver
-import random
 
 @pytest.fixture
 def driver():
@@ -9,16 +8,3 @@ def driver():
     yield driver
     driver.quit()
 
-@pytest.fixture
-def rdata_for_good_reg():
-    new_name = f'Karlos{random.randint(100,999)}'
-    new_email = f'karlos{random.randint(100,999)}@mail.ru'
-    new_password = f'karlos{random.randint(100,999)}'
-    return new_name, new_email, new_password
-
-@pytest.fixture
-def rdata_for_badpass_reg():
-    new_name = f'Karlos{random.randint(100,999)}'
-    new_email = f'karlos{random.randint(100,999)}@mail.ru'
-    new_password = f'k{random.randint(100,999)}'
-    return new_name, new_email, new_password
