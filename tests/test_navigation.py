972298bec422910280f925ec345a568bec14317c
Sergei Karlashov <velanishkis@yandex.ru>
2026-04-20 12:15:50 +0300

    udalil nenugnie importi iz navigation.py

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -1,7 +1,4 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
-import time
+
 from pages.main_page import MainPage
 from pages.base_page import BasePage
 from locators import Locators
